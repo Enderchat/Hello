e6d6aa9da1fb761158b59b29cbfc3a532530a5ef
Enderchat <enderchat2015yandex.ru>
2022-10-28 13:20:09 +0700

    added

---
+++ func.h
@@ -0,0 +1,23 @@
+
+#ifndef _FUNK_H_
+#define _FUNK_H_
+
+
+
+void vstrechayshay();
+void dostavka();
+void proverka_kofe();
+void vibor_oplati(int otvet_klienta);
+void resultat(int otvet_klienta, int money_klienta, int oplata);
+void zakaz();
+void OKONCHANIE();
+void espresso(int oplata,int otvet_klienta,int money_klienta);
+void lungo(int oplata,int otvet_klienta,int money_klienta);
+void doppio(int oplata,int otvet_klienta,int money_klienta);
+void amerikano(int oplata,int otvet_klienta,int money_klienta);
+void kapuchino(int oplata,int otvet_klienta,int money_klienta);
+void latte(int oplata,int otvet_klienta,int money_klienta);
+void kakao(int oplata,int otvet_klienta,int money_klienta);
+void tankeri(int otvet_klienta);
+
+#endif //_FUNC_H_
